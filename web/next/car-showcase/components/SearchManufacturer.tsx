77e1506d3f57b2ebe15cf47f087141c012572676
Santhisenan Ajith <santhisenanajith@gmail.com>
2023-10-09 12:51:30 +0800

    WIP: Next js tutorial

---
+++ web/next/car-showcase/components/SearchManufacturer.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { SearchManufacturerProps } from "@/types";
+import React from "react";
+import Image from "next/image";
+import { useState, Fragment } from "react";
+
+import { Combobox, Transition } from "@headlessui/react";
+
+import { manufacturers } from "@/constants";
+
+const SearchManufacturer = ({
+  manufacturer,
+  setManufacturer,
+}: SearchManufacturerProps) => {
+  const [query, setQuery] = useState("");
+
+  const filteredManufacturers =
+    query === ""
+      ? manufacturers
+      : manufacturers.filter((item) => {
+          // item.toLowerCase().replace(/\s+\g, "").includes(query.toLowerCase().replace(/\s+/g, ""))
+          item.toLowerCase().replace(/\s+/g,"").includes(query.toLowerCase().replace(/\s+/g, ""));
+  return (
+    <div className="search-manufacturer">
+      <Combobox>
+        <div className="relative w-full">
+          <Combobox.Button className="absolute top-[14px]">
+            <Image
+              src="/car-logo.svg"
+              width={20}
+              height={20}
+              className="ml-4"
+              alt="Car Logo"
+            />
+            <Combobox.Input
+              className="search-manufacturer__input"
+              placeholder="Volkswagon"
+              displayValue={(manufacturer: string) => manufacturer}
+              onChange={(e) => setQuery(e.target.value)}
+            />
+          </Combobox.Button>
+        </div>
+      </Combobox>
+
+      <Transition
+        as={Fragment}
+        leave="transition easi-in duration-100"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+        afterLeave={() => setQuery("")}
+      >
+        <Combobox.Options></Combobox.Options>
+      </Transition>
+    </div>
+  );
+};
+
+export default SearchManufacturer;
